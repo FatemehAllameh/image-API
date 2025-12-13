1271bf5caef17a90dcef1cfdaa3402f665e0d417
FatemehAllameh <fatemeh.allameh19@gmail.com>
2025-12-13 16:14:01 +0330

    Add loading and error handling states
    Implemented loading indicator and error messages for API requests.

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+// ELEMENTS
+const loadingImage = document.querySelector(".loading-img");
+const errorMessage = document.querySelector(".error-message");
+const pagination = document.querySelector(".pagination");
+
+// API DATA
 const API_KEY = "-YY31AzwG_-O8OwqzNv-sIalvtupZMnQVZenixjoej8";
 const API_URL = `https://api.unsplash.com/photos/?client_id=${API_KEY}`;
 
 const getPhotos = async (url) => {
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    errorMessage.style.display = "none";
+    loadingImage.style.display = "none";
+    pagination.style.display = "flex";
+
+    showPhotos(data);
+  } catch (err) {
+    errorMessage.style.display = "block";
+    errorMessage.innerHTML = err.message;
+    loadingImage.style.display = "none";
+    pagination.style.display = "none";
+  }
 };
+
+const showPhotos = () => {};
+
 getPhotos(API_URL);
