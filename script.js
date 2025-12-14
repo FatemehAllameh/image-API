ad86765eae20632fa70bc58416a4f87e3bf074b9
FatemehAllameh <fatemeh.allameh19@gmail.com>
2025-12-14 17:31:28 +0330

    Handle search loading, results, and empty state
    Show loading indicator during search, render results after fetch, and display a not-found image when no results are returned.

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 // ELEMENTS
 const loadingImage = document.querySelector(".loading-img");
 const errorMessage = document.querySelector(".error-message");
+const notFoundImage = document.querySelector(".not-found-img");
+const condition = document.querySelector(".condition");
 const imagesList = document.querySelector(".images-list");
 const pagination = document.querySelector(".pagination");
+const searchForm = document.querySelector(".search-form");
+const searchInput = document.querySelector(".search-input");
 
 // API DATA
 const API_KEY = "-YY31AzwG_-O8OwqzNv-sIalvtupZMnQVZenixjoej8";
 const API_URL = `https://api.unsplash.com/photos/?client_id=${API_KEY}`;
+const API_SEARCH = `https://api.unsplash.com/search/photos?page=1&client_id=${API_KEY}&query=`;
 
 const getPhotos = async (url) => {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    errorMessage.style.display = "none";
-    loadingImage.style.display = "none";
+
+    condition.style.display = "none";
+    imagesList.style.display = "grid";
     pagination.style.display = "flex";
 
     showPhotos(data);
@@ -25,14 +31,34 @@ const getPhotos = async (url) => {
   }
 };
 
-const showPhotos = (photos) => {
+const showPhotos = (data) => {
   imagesList.innerHTML = "";
-  photos.forEach((photo) => {
-    const { urls } = photo;
-    const image = `
+  const photos = Array.isArray(data) ? data : data.results;
+  if (photos.length) {
+    photos.forEach((photo) => {
+      const { urls } = photo;
+      const image = `
         <img src="${urls.regular}" alt="image" class="image" />`;
-    imagesList.innerHTML += image;
-  });
+      imagesList.innerHTML += image;
+    });
+  } else {
+    notFoundImage.style.display = "block";
+    pagination.style.display = "none";
+  }
 };
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchValue = searchInput.value;
+  console.log(API_SEARCH + searchValue);
+  if (searchValue) {
+    searchInput.value = "";
+    loadingImage.style.display = "block";
+    notFoundImage.style.display = "none";
+    imagesList.style.display = "none";
+    pagination.style.display = "none";
+    getPhotos(API_SEARCH + searchValue);
+  }
+});
 
 getPhotos(API_URL);
